68e64b973070646c531764aab7d17cab659e6a15
Daniel <chino_reload@hotmail.com>
2018-01-22 23:35:27 -0700

    add validation to the colection model's fields.

--- database/models/local/collection.js
+++ database/models/local/collection.js
@@ -4,11 +4,21 @@ module.exports = (sequelize, DataTypes) => {
   let collection = sequelize.define('collection', {
     name:{
       type: DataTypes.STRING,
-      primaryKey:true
+      primaryKey:true,
+      validate:{
+        notEmpty:{
+          msg:"Name cannot be empty"
+        }
+      }
     },
     path:{
       type:DataTypes.TEXT,
-      allowNull:false
+      allowNull:false,
+      validate:{
+        notEmpty:{
+          msg:"Path cannot be empty"
+        }
+      }
     },
     description:{
       type:DataTypes.TEXT
