e22505cbcde969f7f5e30751fd72b0d1764f58de
urekMazino <chino_reload@hotmail.com>
2018-01-29 23:02:33 -0700

    added parse torrent name by default.

--- moviesFinder.js
+++ moviesFinder.js
@@ -16,14 +16,27 @@ module.exports.findMovies = function(localDB,collection){
     fileFinder.find(collection.get().path)
     .then(function(movieFilesInfo){
       movieFilesInfo.forEach((movieFileInfo)=>{
-        dbInterface.createMovie(movieFileInfo)
+        dbInterface.createMovie(parseInfo(movieFileInfo))
         .then(movie=>{
           dbInterface.addMovieToCollection(collection,movie);
         });
       });
     })
     .catch((err)=>{
-      console.log("wtf");
       console.log(err);
     });
 };
+
+function parseInfo(info){
+  let parsedName = parseTorrentName(info.name);
+  info.name = parsedName.title;
+  info.year = parsedName.year;
+  if (parsedName.season && parsedName.episode){
+    info.name += " S"+zeroPad(parsedName.season,2)+"E"+zeroPad(parsedName.episode,2);
+  }
+  return info;
+}
+function zeroPad(num, places) {
+  var zero = places - num.toString().length + 1;
+  return Array(+(zero > 0 && zero)).join("0") + num;
+}
