7807834543e1d00447696b90b357c6edeeb32960
urekMazino <chino_reload@hotmail.com>
2018-02-07 20:29:21 -0700

    allow parameter on post request to toggle fixing file names on create.

--- fileLookUp/moviesFinder.js
+++ fileLookUp/moviesFinder.js
@@ -9,14 +9,17 @@ const parseTorrentName = require("parse-torrent-name");
 // module.exports.isDirectory = function(path){
 //   return fileFinder.isDirectory(path);
 // }
-module.exports.findMovies = function(localDB,collection){
+module.exports.findMovies = function(localDB,collection,options){
     // imdbLooker.setDB(imdbDB);
     dbInterface.setDB(localDB);
 
     fileFinder.find(collection.get().path)
     .then(function(movieFilesInfo){
       movieFilesInfo.forEach((movieFileInfo)=>{
-        dbInterface.createMovie(parseInfo(movieFileInfo))
+        parseInfo(movieFileInfo,options)
+        .then(movieFileInfo=>{
+          return dbInterface.createMovie(movieFileInfo);
+        })
         .then(movie=>{
           dbInterface.addMovieToCollection(collection,movie);
         });
@@ -27,14 +30,20 @@ module.exports.findMovies = function(localDB,collection){
     });
 };
 
-function parseInfo(info){
-  let parsedName = parseTorrentName(info.name);
-  info.name = parsedName.title;
-  info.year = parsedName.year;
-  if (parsedName.season && parsedName.episode){
-    info.name += " S"+zeroPad(parsedName.season,2)+"E"+zeroPad(parsedName.episode,2);
-  }
-  return info;
+function parseInfo(info,options){
+  return new Promise((resolve,reject)=>{
+    // console.log("\n\n\n\n\n\n\n\n",options.fixNames,!options.fixNames);
+    if (!(options.fixNames == 'true')){
+      return resolve(info);
+    }
+    let parsedName = parseTorrentName(info.name);
+    info.name = parsedName.title;
+    info.year = parsedName.year;
+    if (parsedName.season && parsedName.episode){
+      info.name += " S"+zeroPad(parsedName.season,2)+"E"+zeroPad(parsedName.episode,2);
+    }
+    resolve(info);
+  })
 }
 function zeroPad(num, places) {
   var zero = places - num.toString().length + 1;
--- server/loadResourcesMiddleware.js
+++ server/loadResourcesMiddleware.js
@@ -5,8 +5,11 @@ function loadResourcesMiddleware(resources,localDB,app){
 
   resources.collection.create.sent=function(req,res,context,next){
     if (context.result){
+      let options={
+        fixNames:req.query.fixNames,
+      }
       app.use('/'+context.result.id,express.static(context.result.path));
-      moviesFinder.findMovies(localDB,context.result);
+      moviesFinder.findMovies(localDB,context.result,req.query);
     }
     next();
   }
