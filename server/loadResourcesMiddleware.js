d252dffe733ee81931fe8498fda3475a1446097b
urekMazino <chino_reload@hotmail.com>
2018-02-10 01:16:15 -0700

    restored resourceMiddleware to filter by genres.

--- server/loadResourcesMiddleware.js
+++ server/loadResourcesMiddleware.js
@@ -13,6 +13,25 @@ function loadResourcesMiddleware(resources,localDB,app){
     }
     next();
   }
+
+  resources.collectionMovie.list.before=function(req,res,context,next){
+    if (req.query.genreId){
+      if (!Array.isArray(req.query.genreId)){
+        req.query.genreId = [req.query.genreId];
+      }
+      context.query.group=["movie.name"];
+      context.query.having= localDB.sequelize.literal('COUNT(distinct `genres`.`id`) = '+req.query.genreId.length);
+      context.query.include.push({
+        model:localDB.genre,
+        required:true,
+        attributes:[],
+        where:{id:req.query.genreId},
+        duplicating:false,
+      });
+    }
+    next();
+  }
+
 }
 
 module.exports = loadResourcesMiddleware;
